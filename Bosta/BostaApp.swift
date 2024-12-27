60adefe0aa0c73279bb3e63777365cf15e786447
sama85 <samayasser8520@gmail.com>
2024-12-28 00:13:25 +0100

    Initial Commit

---
+++ Bosta/BostaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BostaApp.swift
+//  Bosta
+//
+//  Created by Sama on 28/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BostaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BostaUITests/BostaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BostaUITestsLaunchTests.swift
+//  BostaUITests
+//
+//  Created by Sama on 28/12/2024.
+//
+
+import XCTest
+
+class BostaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
